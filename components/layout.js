ed9a0355940989bc9ddea5d8934d05ff9a23257f
Scott Smith <scott@scottosmith.net>
2021-02-27 09:08:15 -0600

    Stop tracking .next, working on recursive stuff

--- components/layout.js
+++ components/layout.js
@@ -1,88 +1,84 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
-const name = 'Scott Smith'
-export const siteTitle = 'SOS'
+const name = "Scott Smith";
+export const siteTitle = "SOS";
 
 export default function Layout({ children, home }) {
-  const width = home ? '30vw' : '90vw' ;
+  const width = home ? "30vw" : "90vw";
 
   return (
     <div>
       <Head>
-        <meta
-          name="description"
-          content="Blog for Scott O. Smith"
-        />
+        <meta name="description" content="Blog for Scott O. Smith" />
         <meta name="og:title" content={siteTitle} />
       </Head>
       <div
         style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: width,
-          padding: '1.5rem .5rem',
+          margin: "0 auto",
+          padding: "1.5rem .5rem",
         }}
       >
-      <header>
-        {home ? (
-          <h1
-            style={{
-              marginBottom: "3rem",
-              marginTop: '2rem',
-              fontSize: '2.5rem'
-            }}
-          >
-            <Link
+        <header>
+          {home ? (
+            <h1
               style={{
-                boxShadow: 'none',
-                color: 'inherit'
+                marginBottom: "3rem",
+                marginTop: "2rem",
+                fontSize: "2.5rem",
               }}
-              href="/"
             >
-              <a style={{color: 'white', marginBottom: '3rem'}}>
-                {siteTitle}
-              </a>
-            </Link>
-          </h1>
-        ) : (
-          <h2
-            style={{
-              marginTop: 0,
-            }}
-          >
-            <Link
+              <Link href="/">
+                <a
+                  style={{
+                    color: "white",
+                    marginBottom: "3rem",
+                    boxShadow: "none",
+                    color: "inherit",
+                  }}
+                >
+                  {siteTitle}
+                </a>
+              </Link>
+            </h1>
+          ) : (
+            <h2
               style={{
-                boxShadow: `none`
+                marginTop: 0,
               }}
-              href="/"
             >
-              <a>
-                &lt; {siteTitle}
-              </a>
-            </Link>
-          </h2>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-      {home && (
-        <footer className="footer">
-          © {new Date().getFullYear()}{` `}
-          <div className="footer-links">
-            <Link href="https://scottosmith.net">
-              <a className="link">Scott Smith</a>
+              <Link href="/">
+                <a
+                  style={{
+                    boxShadow: `none`,
+                  }}
+                >
+                  &lt; {siteTitle}
+                </a>
+              </Link>
+            </h2>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div>
+            <Link href="/">
+              <a>← Back to home</a>
             </Link>
           </div>
-        </footer>
-      )}
+        )}
+        {home && (
+          <footer className="footer">
+            © {new Date().getFullYear()}
+            {` `}
+            <div className="footer-links">
+              <a href="https://scottosmith.net" className="link">
+                Scott Smith
+              </a>
+            </div>
+          </footer>
+        )}
       </div>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
-import Date from '../components/date'
-import Layout, { siteTitle } from '../components/layout'
-import { getSortedPostsData } from '../lib/posts'
+import Date from "../components/date";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
 
 export default function Home({ allPostsData }) {
@@ -25,19 +25,23 @@ export default function Home({ allPostsData }) {
           <header>
             <h4
               style={{
-                margin: '0',
-                marginBottom: '.75rem',
-                display: 'inline-block'
+                margin: "0",
+                marginBottom: ".75rem",
+                display: "inline-block",
               }}
             >
-              <Link style={{ boxShadow: `none` }} href="/posts/[id]" as={`/posts/${id}`}>
-                <a><Date dateString={date} /></a>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a style={{ boxShadow: `none` }}>
+                  <Date dateString={date} />
+                </a>
               </Link>
             </h4>
-            <span>{` `}- {title}</span>
+            <span>
+              {` `}- {title}
+            </span>
           </header>
         </article>
       ))}
     </Layout>
-  )
+  );
 }
