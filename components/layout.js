8f855e7bbe33fc1e96abbd14a175972de75aea96
Scott Smith <scott@scottosmith.net>
2021-02-28 17:52:25 -0600

    Home layout tweaks

--- components/layout.js
+++ components/layout.js
@@ -8,78 +8,74 @@ export default function Layout({ children, home }) {
   const width = home ? "50vw" : "98vw";
 
   return (
-    <div>
+    <>
       <Head>
         <meta name="description" content="Blog for Scott O. Smith" />
         <meta name="og:title" content={siteTitle} />
       </Head>
-      <div
-        style={{
-          margin: "0 auto",
-          padding: "1.5rem .5rem",
-          width: width,
-        }}
-      >
-        <header>
-          {home ? (
-            <h1
-              style={{
-                marginBottom: "3rem",
-                marginTop: "2rem",
-                fontSize: "2.5rem",
-              }}
-            >
+      <div className={`site-container${home ? ' home' : ''}`}>
+        <div className="content">
+          <header>
+            {home ? (
+              <h1
+                style={{
+                  marginBottom: "3rem",
+                  marginTop: "2rem",
+                  fontSize: "2.5rem",
+                }}
+              >
+                <Link href="/">
+                  <a
+                    style={{
+                      color: "white",
+                      marginBottom: "3rem",
+                      boxShadow: "none",
+                      color: "inherit",
+                    }}
+                  >
+                    {siteTitle}
+                  </a>
+                </Link>
+              </h1>
+            ) : (
+              <h2
+                style={{
+                  marginTop: 0,
+                }}
+              >
+                <Link href="/">
+                  <a
+                    style={{
+                      boxShadow: `none`,
+                    }}
+                  >
+                    &lt; {siteTitle}
+                  </a>
+                </Link>
+              </h2>
+            )}
+          </header>
+          <main>{children}</main>
+          {!home && (
+            <div>
               <Link href="/">
-                <a
-                  style={{
-                    color: "white",
-                    marginBottom: "3rem",
-                    boxShadow: "none",
-                    color: "inherit",
-                  }}
-                >
-                  {siteTitle}
-                </a>
+                <a>← Back to home</a>
               </Link>
-            </h1>
-          ) : (
-            <h2
-              style={{
-                marginTop: 0,
-              }}
-            >
-              <Link href="/">
-                <a
-                  style={{
-                    boxShadow: `none`,
-                  }}
-                >
-                  &lt; {siteTitle}
-                </a>
-              </Link>
-            </h2>
-          )}
-        </header>
-        <main>{children}</main>
-        {!home && (
-          <div>
-            <Link href="/">
-              <a>← Back to home</a>
-            </Link>
-          </div>
-        )}
-        {home && (
-          <footer className="footer">
-            © {new Date().getFullYear()}
-            {` `}
-            <div className="footer-links">
-              <a href="https://scottosmith.net" className="link">
-                Scott Smith
-              </a>
             </div>
-          </footer>
-        )}
+          )}
+          {home && (
+            <footer className="footer">
+              © {new Date().getFullYear()}
+              {` `}
+              <div className="footer-links">
+                <a href="https://scottosmith.net" className="link">
+                  {name}
+                </a>
+              </div>
+            </footer>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
