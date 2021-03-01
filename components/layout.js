e3648fb20334630beb7454ff567a6de24e3cc4de
Scott Smith <scott@scottosmith.net>
2021-02-28 18:47:33 -0600

    Layout tweaks, remove <br> tags from markdown

--- components/layout.js
+++ components/layout.js
@@ -5,76 +5,51 @@ const name = "Scott Smith";
 export const siteTitle = "SOS";
 
 export default function Layout({ children, home }) {
-  const width = home ? "50vw" : "98vw";
-
   return (
     <>
       <Head>
         <meta name="description" content="Blog for Scott O. Smith" />
         <meta name="og:title" content={siteTitle} />
       </Head>
-      <div className={`site-container${home ? ' home' : ''}`}>
-        <div className="content">
-          <header>
-            {home ? (
-              <h1
-                style={{
-                  marginBottom: "3rem",
-                  marginTop: "2rem",
-                  fontSize: "2.5rem",
-                }}
-              >
-                <Link href="/">
-                  <a
-                    style={{
-                      color: "white",
-                      marginBottom: "3rem",
-                      boxShadow: "none",
-                      color: "inherit",
-                    }}
-                  >
-                    {siteTitle}
-                  </a>
-                </Link>
-              </h1>
-            ) : (
-              <h2
-                style={{
-                  marginTop: 0,
-                }}
-              >
-                <Link href="/">
-                  <a
-                    style={{
-                      boxShadow: `none`,
-                    }}
-                  >
-                    &lt; {siteTitle}
-                  </a>
-                </Link>
-              </h2>
-            )}
-          </header>
-          <main>{children}</main>
-          {!home && (
-            <div>
+      <div className={`content${home ? ' home' : ''}`}>
+        <header>
+          {home ? (
+            <h1 className="site-title home">
               <Link href="/">
-                <a>← Back to home</a>
-              </Link>
-            </div>
-          )}
-          {home && (
-            <footer className="footer">
-              © {new Date().getFullYear()}
-              {` `}
-              <div className="footer-links">
-                <a href="https://scottosmith.net" className="link">
-                  {name}
+                <a>
+                  {siteTitle}
                 </a>
-              </div>
-            </footer>
+              </Link>
+            </h1>
+          ) : (
+            <h2 className="site-title post">
+              <Link href="/">
+                <a>
+                  &lt; {siteTitle}
+                </a>
+              </Link>
+            </h2>
           )}
-        </div>
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
+        )}
+        {home && (
+          <footer className="footer">
+            © {new Date().getFullYear()}
+            {` `}
+            <div className="footer-links">
+              <a href="https://scottosmith.net" className="link">
+                {name}
+              </a>
+            </div>
+          </footer>
+        )}
       </div>
     </>
   );
