a55a103c334c7495514edf7363183ead0c8fb6c3
Scott Smith <scott@scottosmith.net>
2023-07-30 18:21:19 -0500

    Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import '../styles/global.css'
-import '../styles/site.css'
+import "../styles/global.css";
+import "../styles/site.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
